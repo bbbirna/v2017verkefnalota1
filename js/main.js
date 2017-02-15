67a5e71194505bc104721828f6d120ca8e8fdce2
Judy Njeru <JudyPC@Notandis-MacBook-Air.local>
2017-02-15 14:58:24 +0000

    modifications to main.js file and styling on the front page plus an addition of JSON.js file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,159 @@
-// var r = new XMLHttpRequest();
-// r.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=3c2e3323fb6685239c36ee9312c7bcb0", true);
-// r.onreadystatechange = function () {
-//   if (r.readyState != 4 || r.status != 200) return;
-//   const response = JSON.parse(r.responseText)
-//   const container = document.createElement("div");
-//   const image = document.createElement("img");
-//   const title = document.createElement("h1");
-//   image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path
-//   title.innerHTML = response.original_title;
-//   container.append(image);
-//   container.append(title);
-//   document.body.append(container);
-// };
-// r.send("banana=yellow");
+
+class MovieAPI {
+
+  constructor(APIName){
+    this.APIName = APIName;
+    
+    if (this.APIName=="themoviedb") {
+      this.rootURL="https://api.themoviedb.org/3/movie/";
+      this.token ="6c6774fdc0da477c7a3f3f7c03048117";
+    }
+  }
+
+  getPopularMovies(callback){
+    this.getData('popular', function(results) {
+      callback(results);
+    });
+  }
+
+  getRecentMovies(callback){
+    this.getData('now_playing', function(results) {
+      callback(results);
+    });
+  }
+
+  getComedyMovies(callback){//getter becomes an attribute when you an instance
+    this.getData('upcoming', function(results) {
+      callback(results);
+    });
+  }
+
+  getData(queryCategory, callback){//callback is a parameter that is a function that is used whenever getData is called
+    let AJAX = new XMLHttpRequest();
+    AJAX.open("GET", this.rootURL + queryCategory + '?api_key=' + this.token);
+    // AJAX.setRequestHeader("x-access-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1OGExN2FiNjk1ODFhYzcwMmM0YmM4ZjciLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6Ikp1ZHkgTmplcnUiLCJlbWFpbCI6Im5qZXJ1anVkeTg3QHltYWlsLmNvbSIsInVzZXJuYW1lIjoianVkeV9uamVydSIsInBhc3N3b3JkIjoiJDJhJDA4JGNpbVlsY3V3WHViaEI0T2c3Q3BQRmVFMVJjaENBOGdSQ2tLNWszZDFHUkx6YkVsYXF3WjdDIiwiZG9tYWluIjoidmVmc2tvbGkuaXMiLCJtZXNzYWdlIjoic3R1ZHkgcHJvamVjdFxyXG5cclxuIiwiaWF0IjoxNDg3MDg3Nzg5LCJleHAiOjE0ODcxNzQxODl9.uejVEAYnZGQurhnztGLwdkonAP_YEenVraevyaGeTpc");
+    
+    AJAX.onreadystatechange = function(){
+
+      if(AJAX.readyState != 4 || AJAX.status != 200){
+        return;
+      }
+
+      let results = JSON.parse(AJAX.responseText).results;
+        callback(results);
+    };
+    AJAX.send();
+  }
+}
+
+
+const image_path = "http://image.tmdb.org/t/p/original";
+let theMovieDBInstance = new MovieAPI('themoviedb');
+
+theMovieDBInstance.getPopularMovies(function(results) {
+    console.log(results);
+    for( let i=0; i< results.length; i++){
+      let genres = [];
+      let genreString = "";
+      results[i].genre_ids.forEach(function(id){
+        genres = APIS.genres.filter(function(genre){
+          return genre.id === id;
+        });
+      });//close forEach
+      genres.forEach(function(genre){
+        genreString += "<h4>" +genre.name +"</h4>";
+      });
+
+      let title = results[i].title;
+      let image = results[i].poster_path;
+      let rating = results[i].vote_average;
+     
+
+      let movieTitle = '<div class="skew slide-single columns small-12 large-5 medium-5">\
+                          <img class="image" src="'+image_path + image+'"/> <div id="rogue">\
+                          <h2>'+title+'</h2><h3>Felicity Jones | Diego Luna | Forest Whitaker</h3>\
+                          <h4>'+genreString+'</h4></div>\
+                          <div id="floating-button">\
+                            <h2>'+rating+'</h2>\
+                          </div>\
+                        </div>'
+
+      let wrapper = document.getElementById('popularwrapper');
+      wrapper.innerHTML += movieTitle;
+    }//close for loop
+});
+
+
+theMovieDBInstance.getRecentMovies(function(results) {
+    console.log(results);
+    for( let i=0; i< results.length; i++){
+      let genres = [];
+      let genreString = "";
+      results[i].genre_ids.forEach(function(id){
+        genres = APIS.genres.filter(function(genre){
+          return genre.id === id;
+        });
+      });//close forEach
+      genres.forEach(function(genre){
+        genreString += "<h4>" +genre.name +"</h4>";
+      });
+
+      let title = results[i].title;
+      let image = results[i].poster_path;
+      let rating = results[i].vote_average;
+     
+
+      let movieTitle = '<div class="skew slide-single columns small-12 large-5 medium-5">\
+                          <img class="image" src="'+image_path + image+'"/> <div id="rogue">\
+                          <h2>'+title+'</h2><h3>Felicity Jones | Diego Luna | Forest Whitaker</h3>\
+                          <h4>'+genreString+'</h4></div>\
+                          <div id="floating-button">\
+                            <h2>'+rating+'</h2>\
+                          </div>\
+                        </div>'
+
+      let wrapper = document.getElementById('recentmovieswrapper');
+      wrapper.innerHTML += movieTitle;
+    }//close for loop
+});
+
+
+theMovieDBInstance.getComedyMovies(function(results) {
+    console.log(results);
+    for( let i=0; i< results.length; i++){
+      let genres = [];
+      let genreString = "";
+      results[i].genre_ids.forEach(function(id){
+        genres = APIS.genres.filter(function(genre){
+          return genre.id === id;
+        });
+      });//close forEach
+      genres.forEach(function(genre){
+        genreString += "<h4>" +genre.name +"</h4>";
+      });
+
+      let title = results[i].title;
+      let image = results[i].poster_path;
+      let rating = results[i].vote_average;
+     
+
+      let movieTitle = '<div class="skew slide-single columns small-12 large-5 medium-5">\
+                          <img class="image" src="'+image_path + image+'"/> <div id="rogue">\
+                          <h2>'+title+'</h2><h3>Felicity Jones | Diego Luna | Forest Whitaker</h3>\
+                          <h4>'+genreString+'</h4></div>\
+                          <div id="floating-button">\
+                            <h2>'+rating+'</h2>\
+                          </div>\
+                        </div>'
+
+      let wrapper = document.getElementById('comedywrapper');
+      wrapper.innerHTML += movieTitle;
+    }//close for loop
+});
+
+
+
+
 
 
 
@@ -51,85 +192,10 @@
 
 
 
-//MOVIE LIST
-let movieDatabases = [];
 
 
 
-// get data fromkvikmyndir
-let AJAX = new XMLHttpRequest();
-AJAX.open("GET", "http://api.kvikmyndir.is/upcoming");
-AJAX.setRequestHeader("x-access-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1OGExN2FiNjk1ODFhYzcwMmM0YmM4ZjciLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6Ikp1ZHkgTmplcnUiLCJlbWFpbCI6Im5qZXJ1anVkeTg3QHltYWlsLmNvbSIsInVzZXJuYW1lIjoianVkeV9uamVydSIsInBhc3N3b3JkIjoiJDJhJDA4JGNpbVlsY3V3WHViaEI0T2c3Q3BQRmVFMVJjaENBOGdSQ2tLNWszZDFHUkx6YkVsYXF3WjdDIiwiZG9tYWluIjoidmVmc2tvbGkuaXMiLCJtZXNzYWdlIjoic3R1ZHkgcHJvamVjdFxyXG5cclxuIiwiaWF0IjoxNDg2OTgxMzE3LCJleHAiOjE0ODcwNjc3MTd9.A3CS_PEWOhuas0dt5_cxBBAeiNqJ5XCXYdp515O55KA");
-const renderMovies = function(){
-    if(AJAX.readyState != 4 || AJAX.status != 200){
-        return;
-    }
-    let movieList = JSON.parse(AJAX.responseText)
-    console.log(movieList);
-
-    // movieDB = response;
-    getMovie(movieList);
-    
-}
-AJAX.onreadystatechange = renderMovies
-AJAX.send();
-
-
-
-class Movie {
-    constructor(info){
-        $.extend(this,info);
-    }
-
-    // get title(){
-    //  return this.title;
-    // }
-}
-
-
-function getMovie (movieList){
-
-    for( let movieInfo of movieList){
-        let movieDatabase =new Movie(movieInfo); //create new movie from movie class
-        movieDatabases.push(movieDatabase)//push movieDataBase into the movie databases array
-    }
-
-    for( let movieDatabase of movieDatabases){
-        displayMovie(movieDatabase);
-    }
-
-    console.log(movieDatabases);
-    console.log(movieList[0].title);
-
-function displayMovie(movieDatabase){
-
-// for(i=0; i < 1; i++) {
-    let poster = movieDatabase.poster;
-
-    let moviePoster = '<div class="skew columns small-12 large-5 medium-5"><img class="image" src="'+poster+'"/> <div id="rogue"><h2>Rogue One: Star Wars</h2><h3>Felicity Jones | Diego Luna | Forest Whitaker</h3><h4>Action, Adventure, Sci-FI</h4></div></div>'
-
-    let wrapper = document.getElementById('imagewrapper');
-
-    console.log(moviePoster);
-
-    wrapper.innerHTML += moviePoster;
-
-
- // let image = document.createElement("img");
-
- //  image.src = movieDatabase.poster;
- //  document.querySelector("#movie-poster").appendChild(image);
-
-    // let title = document.createElement("h1");
-    // title.innerHTML = movieDatabase.title;
-    // document.querySelector("#myId").appendChild(title);
-}
-}
-
-
-
-   
-                        
+        
 
 
 
